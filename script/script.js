d2603f79bcf1755853edf0bd00700a79ee2ceab2
Anselmo <anselmo.eneto7@gmail.com>
2026-08-06 16:43:57 -0300

    estrutura inicial do portfólio

---
+++ script/script.js
@@ -0,0 +1,19 @@
+const menu = document.querySelector(".header__menu--hamburguer");
+const navMenu = document.querySelector(".header__menu");
+
+menu.addEventListener("click", () => {
+  navMenu.classList.toggle("is-active");
+});
+
+document.addEventListener("click", (event) => {
+  const clicouNoMenu = navMenu.contains(event.target);
+  const clicouNoBotao = menu.contains(event.target);
+
+  if (!clicouNoMenu && !clicouNoBotao) {
+    navMenu.classList.remove("is-active");
+  }
+
+  navMenu.addEventListener("click", () => {
+    navMenu.classList.remove("is-active");
+  });
+});
